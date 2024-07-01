debfe595e46cca6ebb11851757f1fe2921d987df
송영민 <jakgon@naver.com>
2024-07-01 20:48:37 +0900

    Initial commit

---
+++ src/main/kotlin/kr/chuyong/magcloud_remake/MagcloudRemakeApplication.kt
@@ -0,0 +1,11 @@
+package kr.chuyong.magcloud_remake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagcloudRemakeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MagcloudRemakeApplication>(*args)
+}
---
+++ src/test/kotlin/kr/chuyong/magcloud_remake/MagcloudRemakeApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.chuyong.magcloud_remake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MagcloudRemakeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
